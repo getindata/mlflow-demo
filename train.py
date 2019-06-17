5aecd767b36626eee7886a2f29c06b3450746ce5
MLFlow DEMO <mlflow-ci@getindata.com>
2019-06-17 20:22:27 +0000

    Modify files to import train function from .py file.

--- train.py
+++ train.py
@@ -12,19 +12,22 @@ import mlflow
 import mlflow.sklearn
 
 
-def eval_metrics(actual, pred):
-    rmse = np.sqrt(mean_squared_error(actual, pred))
-    mae = mean_absolute_error(actual, pred)
-    r2 = r2_score(actual, pred)
-    return rmse, mae, r2
+def train(in_alpha, in_l1_ratio):
 
 
-if __name__ == "__main__":
+    def eval_metrics(actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        return rmse, mae, r2
+
+
     warnings.filterwarnings("ignore")
     np.random.seed(40)
 
     # Read the wine-quality csv file (make sure you're running this from the root of MLflow!)
-    wine_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "wine-quality.csv")
+    #  Assumes wine-quality.csv is located in the same folder as the notebook
+    wine_path = "wine-quality.csv"
     data = pd.read_csv(wine_path)
 
     # Split the data into training and test sets. (0.75, 0.25) split.
@@ -36,26 +39,44 @@ if __name__ == "__main__":
     train_y = train[["quality"]]
     test_y = test[["quality"]]
 
-    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
-    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+    # Set default values if no alpha is provided
+    if float(in_alpha) is None:
+        alpha = 0.5
+    else:
+        alpha = float(in_alpha)
 
+    # Set default values if no l1_ratio is provided
+    if float(in_l1_ratio) is None:
+        l1_ratio = 0.5
+    else:
+        l1_ratio = float(in_l1_ratio)
+    mlflow.set_experiment('mlflow_demo')
+    # Useful for multiple runs (only doing one run in this sample notebook)    
     with mlflow.start_run():
+        # Execute ElasticNet
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
         lr.fit(train_x, train_y)
 
+        # Evaluate Metrics
         predicted_qualities = lr.predict(test_x)
-
         (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
+        # Print out metrics
         print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
         print("  RMSE: %s" % rmse)
         print("  MAE: %s" % mae)
         print("  R2: %s" % r2)
 
+        # Log parameter, metrics, and model to MLflow
         mlflow.log_param("alpha", alpha)
         mlflow.log_param("l1_ratio", l1_ratio)
         mlflow.log_metric("rmse", rmse)
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
 
-        mlflow.sklearn.log_model(lr, "model")
+        mlflow.sklearn.log_model(lr, "model_wine")
+
+if __name__ == "__main__":
+    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+    train(alpha,l1_ratio)
