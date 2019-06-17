8fb284edc2961b54274d22d94610c5a7453132ed
MLFlow DEMO <mlflow-ci@getindata.com>
2019-06-17 21:16:32 +0000

    Switch model to RandomForest

--- train.py
+++ train.py
@@ -6,13 +6,13 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import ElasticNet
+from sklearn.ensemble import RandomForestRegressor
 
 import mlflow
 import mlflow.sklearn
 
 
-def train(in_alpha, in_l1_ratio):
+def train(in_n_estimators, in_criterion, in_max_depth, in_min_samples_leaf):
 
 
     def eval_metrics(actual, pred):
@@ -39,42 +39,61 @@ def train(in_alpha, in_l1_ratio):
     train_y = train[["quality"]]
     test_y = test[["quality"]]
 
-    # Set default values if no alpha is provided
-    if float(in_alpha) is None:
-        alpha = 0.5
+    # Set default values if no n_trees is provided
+    if int(in_n_estimators) is None:
+        n_estimators = 100
     else:
-        alpha = float(in_alpha)
+        n_estimators = int(in_n_estimators)
 
-    # Set default values if no l1_ratio is provided
-    if float(in_l1_ratio) is None:
-        l1_ratio = 0.5
+    # Set default values if no criterion is provided
+    if str(in_criterion) is None:
+        criterion = 'mse'
     else:
-        l1_ratio = float(in_l1_ratio)
+        criterion = str(in_criterion)
+
+    # Set default values if no n_trees is provided
+    max_depth = int(in_max_depth)
+        
+    # Set default values if no n_trees is provided
+    if int(in_min_samples_leaf) is None:
+        min_samples_leaf = 1
+    else:
+        min_samples_leaf = int(in_min_samples_leaf)
+        
     mlflow.set_experiment('mlflow_demo')
     # Useful for multiple runs (only doing one run in this sample notebook)    
     with mlflow.start_run():
         # Execute ElasticNet
-        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
-        lr.fit(train_x, train_y)
+        rf = RandomForestRegressor(n_estimators=n_estimators,
+                                   criterion=criterion,
+                                   max_depth=max_depth,
+                                   min_samples_leaf=min_samples_leaf)
+        rf.fit(train_x, train_y)
 
         # Evaluate Metrics
-        predicted_qualities = lr.predict(test_x)
+        predicted_qualities = rf.predict(test_x)
         (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
         # Print out metrics
-        print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+        print("RandomForest model (n_estimators={}, criterion={}, max_depth={}, min_samples_leaf={}):".format(n_estimators,
+                                                                                                              criterion,
+                                                                                                              max_depth,
+                                                                                                              min_samples_leaf))
         print("  RMSE: %s" % rmse)
         print("  MAE: %s" % mae)
         print("  R2: %s" % r2)
 
         # Log parameter, metrics, and model to MLflow
-        mlflow.log_param("alpha", alpha)
-        mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_param("n_estimators", n_estimators)
+        mlflow.log_param("criterion", criterion)
+        mlflow.log_param("max_depth", max_depth)
+        mlflow.log_param("min_samples_leaf", min_samples_leaf)        
+        mlflow.log_param("model_type", "RandomForest")
         mlflow.log_metric("rmse", rmse)
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
 
-        mlflow.sklearn.log_model(lr, "model_wine")
+        mlflow.sklearn.log_model(rf, "model_wine")
 
 if __name__ == "__main__":
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
